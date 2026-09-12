f75c9cee0bae09ae4e5df424887f6154a7b18bf2
Ethan Denny <ethandenny@protonmail.com>
2026-09-12 16:53:37 -0230

    Remove unused site sections

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,10 @@
 import { defineConfig } from "astro/config";
 
-import inspectUrls from "@jsdevtools/rehype-url-inspector";
 import tailwind from "@tailwindcss/vite";
 
 export default defineConfig({
   site: "https://ethandenny.dev",
   vite: {
     plugins: [tailwind()],
-  },
-  markdown: {
-    rehypePlugins: [
-      [
-        inspectUrls,
-        {
-          selectors: ["a[href]"],
-          inspectEach(url) {
-            url.node.properties.target = "_blank";
-            url.node.properties.rel = "noopener noreferrer";
-          },
-        },
-      ],
-    ],
   },
 });
